145a5edafb1109284345625aed3096d522cd356d
zhangxianlei <863837949@qq.com>
2018-09-09 14:41:37 +0800

    finished 80%.

--- app/extend/context.js
+++ app/extend/context.js
@@ -3,46 +3,45 @@ const fs = require('fs');
 const basename = require('path').basename;
 const is = require('is-type-of');
 const assert = require('assert');
-const mime = require('mime-types');
 
 module.exports = {
   download(file, name, header) {
     assert(file, 'this parameter: file is required');
     assert(is.string(file), `file must be string, but got ${file}`);
+    fs.access(file, fs.constants.F_OK | fs.constants.R_OK, (err) => {
+      if (err) {
+        throw new Error(`${file} ${err.code === 'ENOENT' ? 'does not exist' : 'is not readable'}`);
+      } else {
+        let reader = fs.createReadStream(file);
+        let filename = basename(file);
 
-    let reader = fs.createReadStream(file);
+        // name
+        if (name) {
+          assert(is.string(name), `name must be string, but got ${name}`);
+          filename = name;
+        }
 
-    let filename = basename(file);
+        this.attachment(filename);
 
-    if (name) {
-        assert(is.string(name), `name must be string, but got ${name}`);
-        filename = name;
-    }
+        // header
+        if (header) {
+          assert(is.object(header), `header must be header, but got ${header}`);
 
-    this.attachment(filename);
-
-    // // mine-type
-    // const mineType = mime.lookup(file);
-    // assert(mineType !== false, `mine-type error, got ${filename}`);
-    // this.set('Content-Type', mineType);
-
-    // header
-    if (header) {
-        assert(is.object(header), `header must be header, but got ${header}`);
-
-        for (let key in header) {
+          let item;
+          for (let key in header) {
+            item = header[key];
             // 首字母大写
             key = key.split('-').map(ucfirst).join('-');
-            this.set(key, header[key]);
+            this.set(key, item);
+          }
         }
-    }
 
-    this.body = reader;
-
-    return true;
+        this.body = reader;
+      }
+    });
   },
 };
 
 function ucfirst(str) {
-    return str.slice(0, 1).toUpperCase() + str.slice(1);
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
 }
