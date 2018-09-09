7ee58ac130261565f66d25dbfd901d721b502214
zhangxianlei <863837949@qq.com>
2018-09-09 12:04:58 +0800

    Basic function finished

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,24 +1,48 @@
 // app/extend/context.js
 const fs = require('fs');
-const path=require('path');
+const basename = require('path').basename;
+const is = require('is-type-of');
+const assert = require('assert');
+const mime = require('mime-types');
 
 module.exports = {
-  download(file, name, header, disposition) {
-    // todo check file
-    let reader = fs.createReadStream(file);
-    let extname=path.extname(file);    //获取文件的后缀名
-    // todo get filename
-    let filename = '' + extname;
-    // header
-    // this.set('Content-Type', 'text/js');
+  download(file, name, header) {
+    assert(file, 'this parameter: file is required');
+    assert(is.string(file), `file must be string, but got ${file}`);
 
-    const disposition = disposition || 'attachment';
-    
+    let reader = fs.createReadStream(file);
+
+    let filename = basename(file);
+
+    if (name) {
+        assert(is.string(name), `name must be string, but got ${name}`);
+        filename = name;
+    }
+
     this.attachment(filename);
-    // this.set('Content-disposition', `attachment;filename=${filename}`); // 设置你的文件名
+
+    // // mine-type
+    // const mineType = mime.lookup(file);
+    // assert(mineType !== false, `mine-type error, got ${filename}`);
+    // this.set('Content-Type', mineType);
+
+    // header
+    if (header) {
+        assert(is.object(header), `header must be header, but got ${header}`);
+
+        for (let key in header) {
+            // 首字母大写
+            key = key.split('-').map(ucfirst).join('-');
+            this.set(key, header[key]);
+        }
+    }
 
     this.body = reader;
 
     return true;
   },
 };
+
+function ucfirst(str) {
+    return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
